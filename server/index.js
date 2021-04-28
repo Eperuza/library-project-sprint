c2642f655561b10b6e496dac727f2698d5ed1cf9
Eperuza <jeremy.riv@outlook.com>
2021-04-28 17:44:46 -0400

    Database started, docker compose file created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const books = require('./books.json');
+// const books = require('./books.json');
 const cors = require('cors')
 
 app.use(cors());
